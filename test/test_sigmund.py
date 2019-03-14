538d7a2fcc57b7d2b23b47f7f2c1b60e8d966a17
Leo Basov <basov.leo@gmail.com>
2019-03-14 15:29:55 +0100

    Added function test_sigmund.TestSigmund.test_total_yield_high_energy

--- test/test_sigmund.py
+++ test/test_sigmund.py
@@ -1,5 +1,20 @@
 import unittest
+import sys
+
+sys.path.append('../.')
+
+import sputtering.sigmund as sigmund
 
 class TestSigmund(unittest.TestCase):
 	def test_total_yield_high_energy(self):
-		pass
+		alpha_apos = 1
+		nuclear_cross_section = 1
+		surface_binding_energy = 1
+		ref_value = 0.042
+
+		calculated_value = sigmund.total_yield_high_energy(alpha_apos, nuclear_cross_section, surface_binding_energy)
+
+		self.assertEqual(ref_value, calculated_value)
+
+if __name__ == '__main__':
+	unittest.main()
