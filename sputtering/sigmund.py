06272814d43351882b6705964364e9acba24394d
Leo Basov <basov.leo@gmail.com>
2019-03-14 15:21:27 +0100

    Added function total_yield_high_energy.

--- sputtering/sigmund.py
+++ sputtering/sigmund.py
@@ -2,6 +2,32 @@
 
 """
 
+def total_yield_high_energy(alpha_apos, nuclear_cross_section, surface_binding_energy):
+	"""Sigmunds total yield formula for E > 1 keV
+
+	This function calculates the total yield for incidend energies > 1 keV.
+
+	Parameters
+    ----------
+	alpha_apos : double
+		Parameter calculated by function...
+
+	nuclear_cross_section : double
+		Nuclear cross section as calculated by the Thomas-Fermi nuclear cross-section formula for high energy (ε >1).
+
+	surface_binding_energy : double
+		Surface binding energy is a tabular value specific for the sputtered medium.
+
+	Returns
+    -------
+    double
+    	The return value is the total yield per incident particle.
+
+    	Ther yield is normaly provided in unit [atoms/incident ion]
+
+	"""
+	return 0.042*alpha_apos*nuclear_cross_section/surface_binding_energy
+
 __author__ = "Leo Basov"
 __copyright__ = "Copyright (C) 2019, Leo Basov"
 __license__ = "GPLv3"
