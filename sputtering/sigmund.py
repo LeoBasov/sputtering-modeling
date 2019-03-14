27b3c27674e2628ee1d31a7a0983a621171895ea
Leo Basov <basov.leo@gmail.com>
2019-03-14 15:03:43 +0100

    Added sigmund module.

---
+++ sputtering/sigmund.py
@@ -0,0 +1,7 @@
+"""This is the base module containing the Sigmund Total Yield Formula and all associated functions
+
+"""
+
+__author__ = "Leo Basov"
+__copyright__ = "Copyright (C) 2019, Leo Basov"
+__license__ = "GPLv3"
